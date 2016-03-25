0ece7c4083bacb6357224015a2d16067772ab715
ShihYi <brvast@gmail.com>
2016-03-25 16:32:32 +0800

    add comment edit and delete function

--- app/controllers/post_comments_controller.rb
+++ app/controllers/post_comments_controller.rb
@@ -7,4 +7,37 @@ class PostCommentsController < ApplicationController
 
 
   end
+
+  def edit
+
+    @post = Post.find( params[:post_id] )
+    @comment = @post.comments.find( params[:id] )
+
+  end
+
+  def update
+    @post = Post.find(params[:post_id] )
+    @comment = @post.comments.find( params[:id] )
+
+    if @comment.update( comment_params )
+      redirect_to post_url( @post )
+    else
+      render :action => :edit
+    end
+
+  end
+
+  def destroy
+    @post = Post.find(params[:post_id] )
+    @comment = @post.comments.find( params[:id] )
+    @comment.destroy
+
+    redirect_to post_url(@post)
+  end
+
+  protected
+
+  def comment_params
+    params.require(:comment).permit(:name,:body)
+  end
 end
