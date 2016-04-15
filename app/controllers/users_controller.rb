98d526f3aebf3aff57e08afd0a1158ee4513b2a2
ShihYi <brvast@gmail.com>
2016-04-15 17:16:03 +0800

    show user commented posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+
+    @user_commented_posts = Post.where(id: current_user.comments.pluck(:post_id).uniq)
   end
 
   private
